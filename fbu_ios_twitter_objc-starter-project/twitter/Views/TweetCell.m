59fe91b357aa51f85b33706a240a05a759ba8948
Jamie Tan <jamietan@fb.com>
2018-07-05 09:41:57 -0700

    Added favorite and retweet

--- fbu_ios_twitter_objc-starter-project/twitter/Views/TweetCell.m
+++ fbu_ios_twitter_objc-starter-project/twitter/Views/TweetCell.m
@@ -8,6 +8,7 @@
 
 #import "TweetCell.h"
 #import <AFNetworking/UIImageView+AFNetworking.h>
+#import "APIManager.h"
 
 @implementation TweetCell
 
@@ -40,4 +41,68 @@
     self.repliesLabel.text = [NSString stringWithFormat:@"%d", tweet.replyCount];
 }
 
+- (IBAction)didTapFavorite:(id)sender {
+    if (self.tweet.favorited) {
+        // Procedure to unfavorite
+        self.tweet.favorited = NO;
+        self.tweet.favoriteCount -= 1;
+        [[APIManager shared] unfavorite:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if (error) {
+                NSLog(@"Error composing Tweet: %@", error.localizedDescription);
+            } else {
+                NSLog(@"Successfully unfavorited the following Tweet: %@", tweet.text);
+            }
+        }];
+    }
+    else {
+        // Procedure to favorite
+        self.tweet.favorited = YES;
+        self.tweet.favoriteCount += 1;
+        [[APIManager shared] favorite:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if (error) {
+                NSLog(@"Error composing Tweet: %@", error.localizedDescription);
+            } else {
+                NSLog(@"Successfully favorited the following Tweet: %@", tweet.text);
+            }
+        }];
+    }
+    
+    [self refreshData];
+}
+
+- (IBAction)didTapRetweet:(id)sender {
+    if (self.tweet.retweeted) {
+        // Procedure to unretweet
+        self.tweet.retweeted = NO;
+        self.tweet.retweetCount -= 1;
+        [[APIManager shared] unretweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if (error) {
+                NSLog(@"Error composing Tweet: %@", error.localizedDescription);
+            } else {
+                NSLog(@"Successfully unretweeted the following Tweet: %@", tweet.text);
+            }
+        }];
+    }
+    else {
+        // Procedure to retweet
+        self.tweet.retweeted = YES;
+        self.tweet.retweetCount += 1;
+        [[APIManager shared] retweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if (error) {
+                NSLog(@"Error composing Tweet: %@", error.localizedDescription);
+            } else {
+                NSLog(@"Successfully retweet the following Tweet: %@", tweet.text);
+            }
+        }];
+    }
+    
+    [self refreshData];
+}
+
+
+-(void) refreshData {
+    self.retweetsLabel.text = [NSString stringWithFormat:@"%d", self.tweet.retweetCount];
+    self.likesLabel.text = [NSString stringWithFormat:@"%d", self.tweet.favoriteCount];
+    self.repliesLabel.text = [NSString stringWithFormat:@"%d", self.tweet.replyCount];
+}
 @end
